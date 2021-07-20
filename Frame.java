ac1610e8703f49f514b531fa2f5185808a040ef3
Bunea Alexandru <alexandrubunea.contact@gmail.com>
2021-07-20 05:58:39 +0300

    first commit

---
+++ Frame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Frame extends JFrame {
+    // constructor
+    Frame() {
+        
+    }
+}
---
+++ Panel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Panel extends JPanel implements Runnable {
+    // constructor
+    Panel() {
+
+    }
+
+    // game-loop
+    @Override
+    public void run() {
+
+    }
+}
